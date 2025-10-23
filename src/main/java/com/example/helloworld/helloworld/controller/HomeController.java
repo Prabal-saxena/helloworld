e5544450b422abd6dbfcd6995c64c967e02d76cf
Prabal-saxena <prabalsaxena98@gmail.com>
2025-10-22 23:41:54 -0400

    Using External Secret in code

--- src/main/java/com/example/helloworld/helloworld/controller/HomeController.java
+++ src/main/java/com/example/helloworld/helloworld/controller/HomeController.java
@@ -1,15 +1,26 @@
 package com.example.helloworld.helloworld.controller;
 
+import com.example.helloworld.helloworld.service.Home;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping
+    @Autowired
+    private Home service;
+
+    @GetMapping("/home")
     public ResponseEntity<String> home(){
+        service.printPassword();
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/example/helloworld/helloworld/service/Home.java
@@ -0,0 +1,19 @@
+package com.example.helloworld.helloworld.service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class Home {
+
+    @Value("${DATABASE_PASSWORD}")
+    private String dbPassword;
+
+    @PostConstruct
+    public void printPassword(){
+        System.out.println(dbPassword);
+    }
+}
